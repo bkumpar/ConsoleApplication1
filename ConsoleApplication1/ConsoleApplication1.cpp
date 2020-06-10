bdaac3e04182e266a3e93f3424a856436ef229b5
bkumpar <bkumpar@gmail.com>
2020-06-10 11:56:38 +0200

    First commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Test VS2105 on WIN 10 pro" << std::endl;
+    return 0;
+}
+

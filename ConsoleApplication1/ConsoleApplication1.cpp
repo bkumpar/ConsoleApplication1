cc820fc5fbdaf2a01e03497992eb445a761df2bf
bkumpar <bkumpar@gmail.com>
2020-06-10 13:32:53 +0200

    Little changes in README.md

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,11 @@ int main()
 	Message * m1 = new Message(L"Hello world");
 	m1->show();
 	delete m1;
-	Message m2;	m2.setText(L"New text");	m2.show();
+
+	Message m2;
+	m2.setText(L"New text");
+	m2.show();
+
 	return 0;
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ConsoleApplication1
-Test repo, will be deleted
+Test repo, will be deleted when testing finished
